803219bd0d5a714e1e46047c027806e0d3a960b4
Amy Sowash <amy.sowash@exxonmobil.com>
2020-03-04 11:04:40 -0600

    changed app run

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from simple_website import application
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8080)
-        # application.run()
+    #application.run(host='0.0.0.0', port=8080)
+    application.run()
